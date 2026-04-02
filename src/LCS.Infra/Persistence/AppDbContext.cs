4296c7159e6cfb4398b7af5f4471732958336e39
Leandre-Esc <leandre.esc@icloud.com>
2026-04-02 12:36:18 +0200

    Install Microsoft.EntityFrameworkCore and create AppDbContext class

---
+++ src/LCS.Infra/Persistence/AppDbContext.cs
@@ -0,0 +1,15 @@
+using LCS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LCS.Infra.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
